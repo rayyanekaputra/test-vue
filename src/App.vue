8bf1d699ada4d0b9782211cfa8fcbc94d6da8c14
rayyanekaputra <bullzhit99@gmail.com>
2024-11-07 18:42:55 +0800

    first init dari kampus

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script>
+
+export default {
+  data() {
+    return {
+      name: 'John Doe',
+      status: 'pending',
+      tasks: ['Task One', 'Task Two', 'Task Three'],
+      link: 'https://google.com'
+    }
+  }
+}
+
+</script>
+
+<template>
+  <h1>
+    {{ name }}!
+  </h1>
+  <p v-if="status === 'active'">User is active</p>
+  <p v-else-if="status === 'pending'">User is pending</p>
+  <p v-else>User is inactive</p>
+
+  <h3>Task: </h3>
+  <ul>
+    <li v-for="task in tasks" :key="task">
+      {{ task }}
+    </li>
+  </ul>
+  <!-- <a bind:href="link">click me</a> this is the full version -->
+  <a :href="link">click me</a>
+  <button >change status</button>
+</template>
