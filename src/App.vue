5326a9b855325234fb93df65c3c5e85b97177871
Rayyan Eka Putra <rayyan.ekaputra@gmail.com>
2024-11-07 19:26:06 +0800

    compositions API, create dan delete

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,37 @@
 <script setup>
 import { ref } from 'vue';
+//miripji konsepnya useRef
 
-    const name = ref('john doe');
-    const status = ref('active');
-    const tasks = ref(['Task One', 'Task Two', 'Task Three']);
+const name = ref('john doe');
+const status = ref('active');
+const tasks = ref(['Task One', 'Task Two', 'Task Three']);
+const newTask = ref('add task');
 
-    const toggleStatus = () =>{
-      if (status.value === "active") {
-        status.value = "pending";
-        console.log(status.value)
-      } else if (status.value === "pending") {
-        status.value = "inactive";
-        console.log(status.value)
-      } else {
-        status.value = "active"
-        console.log(status.value)
-      }
-    }
+const toggleStatus = () => {
+  if (status.value === "active") {
+    status.value = "pending";
+    console.log(status.value)
+  } else if (status.value === "pending") {
+    status.value = "inactive";
+    console.log(status.value)
+  } else {
+    status.value = "active"
+    console.log(status.value)
+  }
+}
+
+const addTask = () => {
+  if (newTask.value.trim()) {
+    tasks.value.push(newTask.value); //push input form ke array tasks
+    newTask.value = ''; //bersihkan input
+  }
+
+
+
+}
+const deleteTask = (index) => {
+    tasks.value.splice(index, 1)
+  }
 
 </script>
 
@@ -28,10 +43,18 @@ import { ref } from 'vue';
   <p v-else-if="status === 'pending'">User is pending</p>
   <p v-else>User is inactive</p>
 
+  <!-- .prevent itu gunanya untuk hilang preventDefault dari elemen html -->
+  <form @submit.prevent="addTask">
+    <label for="newTask">Add Task</label>
+    <input type="text" id="newTask" name="newTask" v-model="newTask">
+    <button type="submit">SUBMIT</button>
+  </form>
+
   <h3>Task: </h3>
   <ul>
-    <li v-for="task in tasks" :key="task">
-      {{ task }}
+    <li v-for="(task, index) in tasks" :key="task">
+      <span>{{ task }}</span>
+      <button @click="deleteTask(index)">x</button>
     </li>
   </ul>
   <!-- <a bind:href="link">click me</a> this is the full version -->
