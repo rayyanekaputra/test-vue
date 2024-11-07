cb567fb693021b3e5a3b8db90d74af54a5c818cc
Rayyan Eka Putra <rayyan.ekaputra@gmail.com>
2024-11-07 19:01:03 +0800

    options API part 1

--- src/App.vue
+++ src/App.vue
@@ -4,11 +4,25 @@ export default {
   data() {
     return {
       name: 'John Doe',
-      status: 'pending',
+      status: 'active',
       tasks: ['Task One', 'Task Two', 'Task Three'],
       link: 'https://google.com'
     }
-  }
+  },
+  methods: {
+    toggleStatus() {
+      if (this.status === "active") {
+        this.status = "pending";
+        console.log(this.status)
+      } else if (this.status === "pending") {
+        this.status = "inactive";
+        console.log(this.status)
+      } else {
+        this.status = "active"
+        console.log(this.status)
+      }
+    },
+  },
 }
 
 </script>
@@ -29,5 +43,7 @@ export default {
   </ul>
   <!-- <a bind:href="link">click me</a> this is the full version -->
   <a :href="link">click me</a>
-  <button >change status</button>
+  <br />
+  <!-- <button v-on:click="toggleStatus">change status</button> fullversion -->
+  <button @click="toggleStatus">change status</button>
 </template>
